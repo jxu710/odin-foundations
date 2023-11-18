7ccc0eca779ba23bbf3ad40ba7eba711533390fb
Jingru Xu <nicoxu1000@gmail.com>
2023-11-17 21:32:34 -0500

    Add a rainbow mode

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -21,10 +21,34 @@ function addGridItem(size) {
 addGridItem(256);
 // reset button
 
-const resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", function () {
-  const gridItems = document.querySelectorAll(".grid-item");
-  Array.from(gridItems).forEach((item) => {
-    item.style.backgroundColor = "#fff";
+function buttonModes() {
+  const resetButton = document.querySelector("#reset");
+  resetButton.addEventListener("click", function () {
+    const gridItems = document.querySelectorAll(".grid-item");
+    Array.from(gridItems).forEach((item) => {
+      item.style.backgroundColor = "#fff";
+      //   remove rainbow effect when click erase button
+      item.removeEventListener("mouseover", rainbowHover);
+    });
   });
-});
+
+  const rainbowButton = document.querySelector("#rainbow");
+  rainbowButton.addEventListener("click", rainbowMode);
+
+  function rainbowMode() {
+    const gridItems = document.querySelectorAll(".grid-item");
+    Array.from(gridItems).forEach((item) => {
+      item.addEventListener("mouseover", rainbowHover);
+    });
+  }
+
+  function rainbowHover(e) {
+    e.target.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 255
+    )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
+      Math.random() * 255
+    )})`;
+  }
+}
+
+buttonModes();
